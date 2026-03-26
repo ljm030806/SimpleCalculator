1dd3d915d6a1a4f394910ae20223aebd8dea1180
ljm03 <ljm030806@gmail.com>
2026-03-26 15:59:10 +0900

    과제1 코딩과 테스트 완료 1.UI 구성▶TextBox(입력표시, 결과표시), Button(계산) 등을적절히배치합니다.2.숫자입력기능▶숫자Button클릭시TextBox에표시합니다. 2가지방법으로표시3.사칙연산계산기능▶2개의피연산자의입력값을Int로바꾸어더하기계산을수행하고그결과를저장합니다.4.계산결과출력▶계산결과값을문자열로변환하여표시합니다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleCalculator
 {
-    partial class Form1
+    partial class Calculator
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,329 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblName = new Label();
+            txt_Cause = new TextBox();
+            btn_CE = new Button();
+            btn_C = new Button();
+            btn_Del = new Button();
+            btn_Division = new Button();
+            btn_Input7 = new Button();
+            btn_Input8 = new Button();
+            btn_Input9 = new Button();
+            btn_Multiplication = new Button();
+            btn_Input4 = new Button();
+            btn_Input5 = new Button();
+            btn_Input6 = new Button();
+            btn_Minus = new Button();
+            btn_Input1 = new Button();
+            btn_Input2 = new Button();
+            btn_Input3 = new Button();
+            btn_Plus = new Button();
+            btn_ChangeSign = new Button();
+            btn_Input0 = new Button();
+            btn_InputPoint = new Button();
+            btn_InputEquals = new Button();
+            label1 = new Label();
+            txt_Result = new TextBox();
+            SuspendLayout();
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Font = new Font("맑은 고딕", 27F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblName.ForeColor = SystemColors.Highlight;
+            lblName.Location = new Point(48, 34);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(295, 48);
+            lblName.TabIndex = 0;
+            lblName.Text = "SimpleCalculator";
+            // 
+            // txt_Cause
+            // 
+            txt_Cause.Font = new Font("맑은 고딕", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txt_Cause.Location = new Point(48, 99);
+            txt_Cause.Name = "txt_Cause";
+            txt_Cause.Size = new Size(290, 35);
+            txt_Cause.TabIndex = 1;
+            // 
+            // btn_CE
+            // 
+            btn_CE.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_CE.Location = new Point(48, 212);
+            btn_CE.Name = "btn_CE";
+            btn_CE.Size = new Size(68, 41);
+            btn_CE.TabIndex = 3;
+            btn_CE.Text = "CE";
+            btn_CE.UseVisualStyleBackColor = true;
+            // 
+            // btn_C
+            // 
+            btn_C.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_C.Location = new Point(122, 212);
+            btn_C.Name = "btn_C";
+            btn_C.Size = new Size(68, 41);
+            btn_C.TabIndex = 4;
+            btn_C.Text = "C";
+            btn_C.UseVisualStyleBackColor = true;
+            // 
+            // btn_Del
+            // 
+            btn_Del.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Del.Location = new Point(196, 212);
+            btn_Del.Name = "btn_Del";
+            btn_Del.Size = new Size(68, 41);
+            btn_Del.TabIndex = 5;
+            btn_Del.Text = "del";
+            btn_Del.UseVisualStyleBackColor = true;
+            // 
+            // btn_Division
+            // 
+            btn_Division.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Division.Location = new Point(270, 212);
+            btn_Division.Name = "btn_Division";
+            btn_Division.Size = new Size(68, 41);
+            btn_Division.TabIndex = 6;
+            btn_Division.Text = "÷";
+            btn_Division.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input7
+            // 
+            btn_Input7.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input7.Location = new Point(48, 259);
+            btn_Input7.Name = "btn_Input7";
+            btn_Input7.Size = new Size(68, 41);
+            btn_Input7.TabIndex = 7;
+            btn_Input7.Text = "7";
+            btn_Input7.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input8
+            // 
+            btn_Input8.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input8.Location = new Point(122, 259);
+            btn_Input8.Name = "btn_Input8";
+            btn_Input8.Size = new Size(68, 41);
+            btn_Input8.TabIndex = 8;
+            btn_Input8.Text = "8";
+            btn_Input8.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input9
+            // 
+            btn_Input9.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input9.Location = new Point(196, 259);
+            btn_Input9.Name = "btn_Input9";
+            btn_Input9.Size = new Size(68, 41);
+            btn_Input9.TabIndex = 9;
+            btn_Input9.Text = "9";
+            btn_Input9.UseVisualStyleBackColor = true;
+            // 
+            // btn_Multiplication
+            // 
+            btn_Multiplication.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Multiplication.Location = new Point(270, 259);
+            btn_Multiplication.Name = "btn_Multiplication";
+            btn_Multiplication.Size = new Size(68, 41);
+            btn_Multiplication.TabIndex = 10;
+            btn_Multiplication.Text = "X";
+            btn_Multiplication.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input4
+            // 
+            btn_Input4.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input4.Location = new Point(48, 306);
+            btn_Input4.Name = "btn_Input4";
+            btn_Input4.Size = new Size(68, 41);
+            btn_Input4.TabIndex = 11;
+            btn_Input4.Text = "4";
+            btn_Input4.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input5
+            // 
+            btn_Input5.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input5.Location = new Point(122, 306);
+            btn_Input5.Name = "btn_Input5";
+            btn_Input5.Size = new Size(68, 41);
+            btn_Input5.TabIndex = 12;
+            btn_Input5.Text = "5";
+            btn_Input5.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input6
+            // 
+            btn_Input6.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input6.Location = new Point(196, 306);
+            btn_Input6.Name = "btn_Input6";
+            btn_Input6.Size = new Size(68, 41);
+            btn_Input6.TabIndex = 13;
+            btn_Input6.Text = "6";
+            btn_Input6.UseVisualStyleBackColor = true;
+            // 
+            // btn_Minus
+            // 
+            btn_Minus.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Minus.Location = new Point(270, 306);
+            btn_Minus.Name = "btn_Minus";
+            btn_Minus.Size = new Size(68, 41);
+            btn_Minus.TabIndex = 14;
+            btn_Minus.Text = "-";
+            btn_Minus.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input1
+            // 
+            btn_Input1.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input1.Location = new Point(48, 353);
+            btn_Input1.Name = "btn_Input1";
+            btn_Input1.Size = new Size(68, 41);
+            btn_Input1.TabIndex = 15;
+            btn_Input1.Text = "1";
+            btn_Input1.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input2
+            // 
+            btn_Input2.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input2.Location = new Point(122, 353);
+            btn_Input2.Name = "btn_Input2";
+            btn_Input2.Size = new Size(68, 41);
+            btn_Input2.TabIndex = 16;
+            btn_Input2.Text = "2";
+            btn_Input2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input3
+            // 
+            btn_Input3.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input3.Location = new Point(196, 353);
+            btn_Input3.Name = "btn_Input3";
+            btn_Input3.Size = new Size(68, 41);
+            btn_Input3.TabIndex = 17;
+            btn_Input3.Text = "3";
+            btn_Input3.UseVisualStyleBackColor = true;
+            btn_Input3.Click += button15_Click;
+            // 
+            // btn_Plus
+            // 
+            btn_Plus.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Plus.Location = new Point(270, 353);
+            btn_Plus.Name = "btn_Plus";
+            btn_Plus.Size = new Size(68, 41);
+            btn_Plus.TabIndex = 18;
+            btn_Plus.Text = "+";
+            btn_Plus.UseVisualStyleBackColor = true;
+            // 
+            // btn_ChangeSign
+            // 
+            btn_ChangeSign.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_ChangeSign.Location = new Point(48, 400);
+            btn_ChangeSign.Name = "btn_ChangeSign";
+            btn_ChangeSign.Size = new Size(68, 41);
+            btn_ChangeSign.TabIndex = 19;
+            btn_ChangeSign.Text = "+/-";
+            btn_ChangeSign.UseVisualStyleBackColor = true;
+            // 
+            // btn_Input0
+            // 
+            btn_Input0.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_Input0.Location = new Point(122, 397);
+            btn_Input0.Name = "btn_Input0";
+            btn_Input0.Size = new Size(68, 41);
+            btn_Input0.TabIndex = 20;
+            btn_Input0.Text = "0";
+            btn_Input0.UseVisualStyleBackColor = true;
+            // 
+            // btn_InputPoint
+            // 
+            btn_InputPoint.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_InputPoint.Location = new Point(196, 397);
+            btn_InputPoint.Name = "btn_InputPoint";
+            btn_InputPoint.Size = new Size(68, 41);
+            btn_InputPoint.TabIndex = 21;
+            btn_InputPoint.Text = ".";
+            btn_InputPoint.UseVisualStyleBackColor = true;
+            btn_InputPoint.Click += button3_Click;
+            // 
+            // btn_InputEquals
+            // 
+            btn_InputEquals.Font = new Font("맑은 고딕", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btn_InputEquals.Location = new Point(270, 400);
+            btn_InputEquals.Name = "btn_InputEquals";
+            btn_InputEquals.Size = new Size(68, 41);
+            btn_InputEquals.TabIndex = 22;
+            btn_InputEquals.Text = "=";
+            btn_InputEquals.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 23;
+            // 
+            // txt_Result
+            // 
+            txt_Result.Font = new Font("맑은 고딕", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txt_Result.Location = new Point(48, 150);
+            txt_Result.Name = "txt_Result";
+            txt_Result.Size = new Size(290, 35);
+            txt_Result.TabIndex = 24;
+            // 
+            // Calculator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(431, 450);
+            Controls.Add(txt_Result);
+            Controls.Add(label1);
+            Controls.Add(btn_InputEquals);
+            Controls.Add(btn_InputPoint);
+            Controls.Add(btn_Input0);
+            Controls.Add(btn_ChangeSign);
+            Controls.Add(btn_Plus);
+            Controls.Add(btn_Input3);
+            Controls.Add(btn_Input2);
+            Controls.Add(btn_Input1);
+            Controls.Add(btn_Minus);
+            Controls.Add(btn_Input6);
+            Controls.Add(btn_Input5);
+            Controls.Add(btn_Input4);
+            Controls.Add(btn_Multiplication);
+            Controls.Add(btn_Input9);
+            Controls.Add(btn_Input8);
+            Controls.Add(btn_Input7);
+            Controls.Add(btn_Division);
+            Controls.Add(btn_Del);
+            Controls.Add(btn_C);
+            Controls.Add(btn_CE);
+            Controls.Add(txt_Cause);
+            Controls.Add(lblName);
+            Name = "Calculator";
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblName;
+        private TextBox txt_Cause;
+        private Button btn_CE;
+        private Button btn_C;
+        private Button btn_Del;
+        private Button btn_Division;
+        private Button btn_Input7;
+        private Button btn_Input8;
+        private Button btn_Input9;
+        private Button btn_Multiplication;
+        private Button btn_Input4;
+        private Button btn_Input5;
+        private Button btn_Input6;
+        private Button btn_Minus;
+        private Button btn_Input1;
+        private Button btn_Input2;
+        private Button btn_Input3;
+        private Button btn_Plus;
+        private Button btn_ChangeSign;
+        private Button btn_Input0;
+        private Button btn_InputPoint;
+        private Button btn_InputEquals;
+        private Label label1;
+        private TextBox txt_Result;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,183 @@
+using System;
+using System.Windows.Forms;
+
 namespace SimpleCalculator
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        // 계산 상태를 저장할 변수들
+        private double _firstOperand = 0;     // 첫 번째 입력된 숫자
+        private string _currentOperator = ""; // 현재 선택된 연산자
+        private bool _isNewEntry = true;      // 새롭게 숫자를 입력해야 하는지 여부
+        private bool _isCalculated = false;   // '=' 을 눌러 계산이 완료된 직후인지 여부
+
+        public Calculator()
         {
             InitializeComponent();
+
+            // 숫자 버튼 이벤트 일괄 연결
+            btn_Input0.Click += NumberButton_Click;
+            btn_Input1.Click += NumberButton_Click;
+            btn_Input2.Click += NumberButton_Click;
+            btn_Input3.Click += NumberButton_Click;
+            btn_Input4.Click += NumberButton_Click;
+            btn_Input5.Click += NumberButton_Click;
+            btn_Input6.Click += NumberButton_Click;
+            btn_Input7.Click += NumberButton_Click;
+            btn_Input8.Click += NumberButton_Click;
+            btn_Input9.Click += NumberButton_Click;
+
+            // 덧셈 기호 버튼 이벤트 연결
+            btn_Plus.Click += OperatorButton_Click;
+
+            // 결과(=) 및 초기화(C) 버튼 연결
+            btn_InputEquals.Click += EqualsButton_Click;
+            btn_C.Click += ClearButton_Click;
+        }
+
+        // 공통 숫자 버튼 클릭 이벤트 핸들러
+        private void NumberButton_Click(object? sender, EventArgs e)
+        {
+            Button? btn = sender as Button;
+            if (btn != null)
+            {
+                // 계산이 끝난 직후(=)에 새 숫자를 누르면 모든 기록 초기화 후 시작
+                if (_isCalculated)
+                {
+                    txt_Cause.Text = "";
+                    txt_Result.Text = "0";
+                    _isCalculated = false;
+                }
+
+                // 내가 누른 숫자들 내역에 그대로 표시
+                txt_Cause.Text += btn.Text;
+
+                // 연산기호를 누른 직후이거나 초기 상태(_isNewEntry 가 true일 때)
+                if (_isNewEntry)
+                {
+                    txt_Result.Text = btn.Text;
+                    _isNewEntry = false;
+                }
+                else
+                {
+                    // 0만 있는 상태에서 다른 숫자를 누르면 덮어쓰기
+                    if (txt_Result.Text == "0")
+                        txt_Result.Text = btn.Text;
+                    else
+                        txt_Result.Text += btn.Text;
+                }
+            }
+        }
+
+        // 연산(+) 버튼 클릭 이벤트 핸들러
+        private void OperatorButton_Click(object? sender, EventArgs e)
+        {
+            Button? btn = sender as Button;
+            if (btn != null)
+            {
+                // 계산이 완료된 직후(결과값이 나온 상태)에서 연산자를 누르면 내역 이어가기
+                if (_isCalculated)
+                {
+                    txt_Cause.Text = txt_Result.Text;
+                    _isCalculated = false;
+                }
+
+                // 누른 기호를 내역에 표시
+                txt_Cause.Text += btn.Text;
+
+                // 이미 덧셈 기호가 선택되어 있는데 다시 누르는 경우 (임시 계산)
+                if (!_isNewEntry && _currentOperator != "")
+                {
+                    // 내부적으로 계산만 먼저 수행
+                    double.TryParse(txt_Result.Text, out double tempOperand);
+                    _firstOperand = _firstOperand + tempOperand;
+                    txt_Result.Text = _firstOperand.ToString();
+                }
+                else
+                {
+                    // 첫 기호 입력인 경우 현재 값을 피연산자로 저장
+                    double.TryParse(txt_Result.Text, out _firstOperand);
+                }
+
+                _currentOperator = btn.Text;
+                _isNewEntry = true; // 다음은 새로운 숫자를 입력받을 준비
+            }
+        }
+
+        // 등호(=) 버튼 클릭 이벤트 핸들러
+        private void EqualsButton_Click(object? sender, EventArgs e)
+        {
+            if (_currentOperator == "") return;
+
+            // 현재 텍스트박스의 값을 두 번째 피연산자로 가져옴
+            if (double.TryParse(txt_Result.Text, out double secondOperand))
+            {
+                double result = 0;
+
+                // 덧셈 기호에 따라 계산
+                switch (_currentOperator)
+                {
+                    case "+":
+                        result = _firstOperand + secondOperand;
+                        break;
+                }
+
+                // 결과를 텍스트박스에 출력
+                txt_Result.Text = result.ToString();
+                
+                // 내역에도 = 기호와 함께 결과값 표시 (예: "1+2=3")
+                txt_Cause.Text += "=" + result.ToString();
+                
+                // 연속된 연산을 위해 현재 결과를 _firstOperand에 저장
+                _firstOperand = result;
+                _currentOperator = ""; // 연산 종료
+                _isNewEntry = true;    // 다음 값은 새로 입력받음
+                _isCalculated = true;  // 계산 완료 상태로 설정! (다음에 숫자를 누르면 리셋됨)
+            }
+        }
+
+        // 초기화(C) 버튼 클릭 이벤트 핸들러
+        private void ClearButton_Click(object? sender, EventArgs e)
+        {
+            txt_Result.Text = "0";
+            txt_Cause.Text = ""; // 입력 내역 텍스트도 완전히 초기화
+            _firstOperand = 0;
+            _currentOperator = "";
+            _isNewEntry = true;
+            _isCalculated = false;
+        }
+
+        // 소수점 입력 버튼의 이벤트 핸들러 내용
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_isCalculated)
+            {
+                txt_Cause.Text = "";
+                txt_Result.Text = "0";
+                _isCalculated = false;
+            }
+
+            // 연산기호를 막 누른 참이면 "0." 으로 시작
+            if (_isNewEntry)
+            {
+                txt_Cause.Text += "0.";
+                txt_Result.Text = "0.";
+                _isNewEntry = false;
+                return;
+            }
+
+            // 이미 소수점이 찍혀 있다면 무시
+            if (!txt_Result.Text.Contains("."))
+            {
+                txt_Cause.Text += ".";
+                txt_Result.Text += ".";
+            }
+        }
+
+        // 기존에 디자이너에서 더블클릭해서 생성된 이벤트 (유지 권장)
+        private void button15_Click(object sender, EventArgs e)
+        {
+            // 사용하지 않을 경우 Form1.Designer.cs에서 이벤트 연결을 제거해야 합니다.
         }
     }
 }
